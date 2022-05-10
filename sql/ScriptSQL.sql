ae0ff619e1ea7e4a1b4698b374d47701d9107669
Gabriel Longas <gabriel.longas@gmail.com>
2022-05-09 23:36:41 -0300

    Alteração no script de criação do banco de dados

--- sql/ScriptSQL.sql
+++ sql/ScriptSQL.sql
@@ -3,7 +3,7 @@ CREATE DATABASE db_sme
 USE db_sme
 
 CREATE TABLE TipoUser (
-	id_TipoUser int IDENTITY(1,1) NOT NULL PRIMARY KEY,
+	id int IDENTITY(1,1) NOT NULL PRIMARY KEY,
 	desc_tipoUser varchar (max) NOT NULL,
 );
 
@@ -11,17 +11,17 @@ INSERT INTO TipoUser (desc_tipoUser) VALUES('Admin')
 INSERT INTO TipoUser (desc_tipoUser) VALUES('Usuário')
 
 CREATE TABLE Usuarios (
-	id_Usuario int IDENTITY(1,1) NOT NULL PRIMARY KEY,
+	id int IDENTITY(1,1) NOT NULL PRIMARY KEY,
 	Nome varchar(max) NOT NULL,
 	Email varchar(max) NOT NULL,
 	Senha varchar(max) NOT NULL,
 	Imagem varbinary(max) NULL,
 	DataCadastro date NOT NULL,
-	TipoUsuario int NOT NULL FOREIGN KEY REFERENCES TipoUser(id_tipoUser)
+	TipoUsuario int NOT NULL FOREIGN KEY REFERENCES TipoUser(id)
 );
 
 CREATE TABLE TipoVias (
-	id_TipoVia int IDENTITY(1,1) NOT NULL PRIMARY KEY,
+	id int IDENTITY(1,1) NOT NULL PRIMARY KEY,
 	Descricao varchar (max) NOT NULL,
 );
 
@@ -30,22 +30,22 @@ INSERT INTO TipoVias (Descricao) VALUES('Avenida')
 INSERT INTO TipoVias (Descricao) VALUES('Via')
 
 CREATE TABLE Controladores (
-	id_Controlador int IDENTITY(1,1) NOT NULL PRIMARY KEy,
+	id int IDENTITY(1,1) NOT NULL PRIMARY KEY,
 	Nome varchar(max) NOT NULL,
 	Endereco varchar(max) NOT NULL,
 	Numero int NOT NULL,
 	Bairro varchar(max) NOT NULL,
 	Cidade varchar(max) NOT NULL,
 	Estado varchar(max) NOT NULL,
-	TipoVia int NOT NULL FOREIGN KEY REFERENCES TipoVias(id_TipoVia),
+	TipoVia int NOT NULL FOREIGN KEY REFERENCES TipoVias(id),
 	CEP int NOT NULL,
 	Latitude float NOT NULL,
 	Longitude float NOT NULL
 );
 
 CREATE TABLE Registros (
-	id_Registro int IDENTITY(1,1) NOT NULL PRIMARY KEY,
-	Controlador int NOT NULL FOREIGN KEY REFERENCES Controladores(id_Controlador),
+	id int IDENTITY(1,1) NOT NULL PRIMARY KEY,
+	Controlador int NOT NULL FOREIGN KEY REFERENCES Controladores(id),
 	Nivel int NOT NULL,
 	DataHora datetime NOT NULL
 );
